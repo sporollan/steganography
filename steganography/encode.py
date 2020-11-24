8db5a5c70667d2119cd79289064607759bd47e89
sporollan <s.porollan@gmail.com>
2020-11-24 18:45:33 -0300

    open and write image

--- steganography/encode.py
+++ steganography/encode.py
@@ -2,6 +2,7 @@ import argparse
 import multiprocessing as mp
 import cv2 as cv
 
+
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--image', metavar='image', type=str,
@@ -10,5 +11,18 @@ def get_args():
                         help='Message Filename', required=True)
     return parser.parse_args()
 
+
+def show(img):
+    cv.imshow("display", img)
+    cv.waitKey(0)
+
+
 if __name__ == '__main__':
     args = get_args()
+    img = cv.imread(args.image)
+    msg = open(args.message, 'r').read().rstrip()
+
+
+
+    if not cv.imwrite('encoded_image.png', img):
+        raise Exception("Could not write image")
